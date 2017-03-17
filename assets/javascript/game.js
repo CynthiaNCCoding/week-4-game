8047e22393262d25b7e0b6ae50aebcc28674298a
CLRB17 <cynthia@brown-house.net>
2017-03-17 12:48:10 -0400

    it's workinggit add -A  And it's glorious.  Added a pointer finger.  The win/lose alert could be a little more exciting...

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,116 +1,87 @@
- // Assigning Random #'s to gems and start # 
+var generatedNumber, redNumber, blueNumber, yellowNumber, greenNumber;
+var gameStarted = false;
+var countScore;
+var wins = 0;
+var losses = 0;
 
-var generatedNumber = 19 + Math.floor(Math.random() * 101);
-console.log(generatedNumber);
+function startGame() {
+	if (gameStarted) {
+		return;
+	}
 
-var redNumber= 1 + Math.floor(Math.random() * 12);
-redNumber!= blueNumber | yellowNumber | greenNumber;
-console.log(redNumber);
+	$("#endGameAlert").html("");
 
-var blueNumber= 1 + Math.floor(Math.random() * 12);
-blueNumber!= redNumber | yellowNumber | greenNumber;
-console.log(blueNumber);
+	generatedNumber = 19 + Math.floor(Math.random() * 101);
+	$("#random").html(generatedNumber);
+	console.log(generatedNumber);
 
-var yellowNumber= 1 + Math.floor(Math.random() * 12);
-yellowNumber!= redNumber | blueNumber | greenNumber;
-console.log(yellowNumber);
+	redNumber= 1 + Math.floor(Math.random() * 12);
+	console.log(redNumber);
 
-var greenNumber= 1 + Math.floor(Math.random() * 12);
-	greenNumber!= redNumber | blueNumber | yellowNumber;
-console.log(greenNumber);
+	blueNumber= 1 + Math.floor(Math.random() * 12);
+	console.log(blueNumber);
 
-var anyGems = ["redNumber", "blueNumber", "yellowNumber", "greenNumber"];
+	yellowNumber= 1 + Math.floor(Math.random() * 12);
+	console.log(yellowNumber);
 
-var x 
+	greenNumber= 1 + Math.floor(Math.random() * 12);
+	console.log(greenNumber);
 
-var countScoreDiv = $('#countScore');
-var countScoreWrite = countScoreDiv.text();
-var countScoreParse = parseInt(countScoreWrite);
+	countScore = 0;
+	$("#countScore").html(countScore);
+	gameStarted = true;
+}
 
+function gemClicked(event) {
+	if (gameStarted==false) {
+		return;
+	}
+	if ($(this).attr("id") == "red") {
+		countScore = countScore + redNumber;
+	} else if ($(this).attr("id") == "blue") {
+		countScore = countScore + blueNumber;
+	} else if ($(this).attr("id") == "yellow") {
+		countScore = countScore + yellowNumber;
+	} else if ($(this).attr("id") == "green") {
+		countScore = countScore + greenNumber;
+	}
 
+	$("#countScore").html(countScore);
 
-// When START is clicked
+	console.log("you clicked: " + $(this).attr("id"));
+	winLose();
+}
 
-$(document).ready(function(){
-
-
-    $("#startBox").click(function(){
-    	$("#countScore").html(0);
-		$("#random").html(generatedNumber);
-
-		if (redNumber = blueNumber) {
-			redNumber;
-		}
-    });
-
-    // ADD THE GEMS TO COUNTSCORE******
-
-	// $("#red").on("click", function() {
-	// 	redNumber + redNumber = var XX;
-	// 	$("#countScore").html(XX);
-
-	// }
-
-
-
-    // BAD CODE
-	// $("#red").on("click", function() {
-	// 	var updatedScore = countScoreParse + redNumber;
-	// 	// updatedScore = countScoreParse;
-	// 	// countScoreParse = "#countScore";
-	// 	// $("#countScore").html(countScoreParse);
-	// 		$("#countScore").html(updatedScore);
-
-	// 	console.log(countScoreParse);
-	// });
-
-// BAD CODE
-// // When red gem is clicked
-// 	$("#red").on("click", function() {
-// 		getElementById("#countScore").innerHTML(redNumber;=);
-// }
-// });
-
-
-// OLD
-	// $("#red").on("click", function() {
- //    	"#countScore" + redNumber = updatedScore;
- //    	updatedScore = "#countScore";
-	// });
-
-// }); <!--this was the closing bracket for doc.ready-->
-
-// INTERNET******
-// $("#red").click(function() {
-// $("#countScore")++;
-// });
+function winLose() {
+	if (countScore < generatedNumber) {
+		return;
+	}
+	if (countScore == generatedNumber) {
+		$("#endGameAlert").html("You Win!");
+		gameStarted = false;
+		wins++;
+		$("#win").html(wins);
+	}
+	if (countScore > generatedNumber) {
+		$("#endGameAlert").html("You Lose!");
+		gameStarted = false;
+		losses++;
+		$("#lose").html(losses);
+	}
+}
 
 
-// // When blue gem is clicked
-
-	$("#blue").on("click", function() {
-    	$("#countScore").html(blueNumber);
-	});
-
-// // When yellow gem is clicked
-
-	$("#yellow").on("click", function() {
-    	$("#countScore").html(yellowNumber);
-	});
-
-// // When green gem is clicked
-
-	$("#green").on("click", function() {
-    	$("#countScore").html(greenNumber);
-	});
-
+$(document).ready(function() {
+	$("#startBox").on("click", startGame);
+	$(".gem").on("click", gemClicked);	
 });
 
-// ---------------
-// var jLikes = $('div.likes');
-// var sLikes = jLikes.text();
-// var nLikes = parseInt(sLikes);
-// if(isNaN(nLikes)) {
-//    return;
-// }
-// jLikes.text(nLikes+1);
+
+//     $("#startBox").click(function(){
+//     	$("#countScore").html(0);
+// 		$("#random").html(generatedNumber);
+
+// 		if (redNumber = blueNumber) {
+// 			redNumber;
+// 		}
+//     });
